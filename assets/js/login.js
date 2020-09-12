733140afa10faa8c4efe1f8484488bc9245a7783
syq <1391903654@qq.com>
2020-09-12 14:39:14 +0800

    这是我的大事件

---
+++ assets/js/login.js
@@ -0,0 +1,50 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    var form = layui.form
+    var layer=layui.layer
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+    $('#form_reg').on('submit',function(e){
+       e.preventDefault()
+       var data=$(this).serialize()
+       $.post('/api/reguser',data,function(res){
+         if(res.status!==0){
+             return layer.msg(res.message)
+         }
+         layer.msg('注册成功，请登录')
+         $('#link_login').click()
+       })
+    })
+    $('#form_login').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+          url: '/api/login',
+          method: 'POST',
+          data: $(this).serialize(),
+          success: function(res) {
+            if (res.status !== 0) {
+              return layer.msg('登录失败！')
+            }
+            layer.msg('登录成功！')
+            // 将登录成功得到的 token 字符串，保存到 localStorage 中
+            localStorage.setItem('token', res.token)
+            location.href = '/index.html'
+          }
+        })
+      })
+})
